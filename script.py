b504dc0e8e0d98f6d89a96cbcb0eeb2868584cb8
Daniel Eduardo Chavez Vasquez <chavezde@ecuador.h.local>
2024-12-06 17:27:30 -0500

    falta depurar zona en inv_naps

--- script.py
+++ script.py
@@ -1,25 +1,25 @@
 import time
 import logging
 import sys
-from sources import comparate, naps  
+from sources import comparacion, inv_naps  
 
 # Configuración del logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def ejecutar_scripts():
     """
-    Ejecuta los scripts comparate.py y naps.py secuencialmente.
+    Ejecuta los scripts comparacion.py y inv_naps.py secuencialmente.
     """
-    logging.info('Iniciando ejecución de comparate.py')
-    print("\n=== Iniciando ejecución de comparate.py ===")
-    comparate.main()
+    logging.info('Iniciando ejecución de comparacion.py')
+    print("\n=== Iniciando ejecución de comparacion.py ===")
+    comparacion.main()
     
     # Pausa para asegurar que el archivo CSV se haya generado
     time.sleep(1)
     
-    logging.info('Iniciando ejecución de naps.py')
-    print("\n=== Iniciando ejecución de naps.py ===")
-    naps.main()
+    logging.info('Iniciando ejecución de inv_naps.py')
+    print("\n=== Iniciando ejecución de inv_naps.py ===")
+    inv_naps.main()
 
 if __name__ == '__main__':
     # Agrega el directorio 'sources' al path de Python
